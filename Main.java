aed76eac24596d4fb3d113dbe106281192ae6301
Nick Norton <nnort9879@gmail.com>
2019-09-25 09:41:22 -0400

    print thing that looks like board

--- Main.java
+++ Main.java
@@ -5,10 +5,20 @@
 
 public class Main {
 
-public static void main ( String[] args) {
+	public static void main ( String[] args) {
+		int[] row1 = {0,0,0,0,0}
+		int[] row2 = {0,0,0,0,0}
+		int[] row3 = {0,0,0,0,0}
+		int[] row4 = {0,0,0,0,0}
+		int[] row5 = {0,0,0,0,0}
+		System.out.printf("%s    %s    %s    %s    %s", "o", "o", "o", "o", "o");
+		System.out.printf("%s    %s    %s    %s    %s", "o", "o", "o", "o", "o");
+		System.out.printf("%s    %s    %s    %s    %s", "o", "o", "o", "o", "o");
+		System.out.printf("%s    %s    %s    %s    %s", "o", "o", "o", "o", "o");
+		System.out.printf("%s    %s    %s    %s    %s", "o", "o", "o", "o", "o");
+		System.out.println();
+		
+	}  //close main method
 
-
-}  //close main method
-
-} . //close the Main class
+} //close the Main class
 
